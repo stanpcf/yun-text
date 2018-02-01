2a589196f5f8bb4b25fac8b6c6b6010a8f431dd2
stanpcf <fpchappy@163.com>
2018-02-01 21:13:29 +0800

    refactor some code

--- result_analysis.py
+++ result_analysis.py
@@ -1,32 +1,36 @@
 # coding: utf-8
-import argparse
 import numpy as np
 import pandas as pd
+from collections import namedtuple
 
-_usage = "ipython -i result_analysis.py h5_file"
-_parser = argparse.ArgumentParser(description="预测结果分析工具.", usage=_usage)
-_parser.add_argument("h5_path", type=str,
-                     help="训练完网络的h5的路径. 例如 bidirectional_lstm/BiLSTM_softmax_50_300_80_2018013014.h5")
-_args = _parser.parse_args()
-
-_d, _f = _args.h5_path.split(".")[0].split("/")
-
-valid_prd_file = _d + "/result/valid_" + _f + ".csv"
-test_prd_file = _d + "/result/" + _f + ".csv"
-
-valid_prd = pd.read_csv(valid_prd_file, header=None, names=['Id', 'prd'])
-test_prd = pd.read_csv(test_prd_file, header=None, names=['Id', 'prd'])
+"""
+usage: ipython -i result_analysis.py
+"""
 
 train = pd.read_csv("./input/processed/train_first.csv")
 test = pd.read_csv("./input/processed/predict_first.csv")
 
-valid_merge = pd.merge(valid_prd, train, how='left', on=['Id'])
-valid_merge = valid_merge[['Id', 'prd', 'Score', 'Discuss_processed', 'Discuss']]
-valid_diff = valid_merge[np.abs(valid_merge.prd-valid_merge.Score) > 1]
 
-test_merge = pd.merge(test_prd, test, how='left', on=['Id'])
-test_merge = test_merge[['Id', 'prd', 'Discuss_processed', 'Discuss']]
-test_low = test_merge[test_merge.prd <= 3]
+def get_result_stat(h5_path):
+    """
+    :param h5_path: 训练完之后的h5_file路径
+    bidirectional_lstm/BiLSTM_softmax_50_300_80_2018013014.h5
+    """
+    _d, _f = h5_path.split(".")[0].split("/")
 
+    valid_prd_file = _d + "/result/valid_" + _f + ".csv"
+    test_prd_file = _d + "/result/" + _f + ".csv"
 
-del _args, _parser, _d, _f, _usage
+    valid_prd = pd.read_csv(valid_prd_file, header=None, names=['Id', 'prd'])
+    test_prd = pd.read_csv(test_prd_file, header=None, names=['Id', 'prd'])
+
+    valid_merge = pd.merge(valid_prd, train, how='left', on=['Id'])
+    valid_merge = valid_merge[['Id', 'prd', 'Score', 'fool', 'Discuss']]
+    valid_diff = valid_merge[np.abs(valid_merge.prd-valid_merge.Score) > 1]
+
+    test_merge = pd.merge(test_prd, test, how='left', on=['Id'])
+    test_merge = test_merge[['Id', 'prd', 'fool', 'Discuss']]
+    test_low = test_merge[test_merge.prd <= 3]
+    ResultStat = namedtuple("ResultStat", ["valid_merge", "valid_diff", "test_merge", "test_low"])
+    result = ResultStat(valid_merge=valid_merge, valid_diff=valid_diff, test_merge=test_merge, test_low=test_low)
+    return result
