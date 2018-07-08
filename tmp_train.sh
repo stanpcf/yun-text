9f2cec3ea1f488639027dbb89cd3887006e10753
stanpcf <fpchappy@163.com>
2018-07-09 01:07:11 +0800

    delete some script

--- tmp_train.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-python train.py --classifier=textcnn.TextCNNMultiKernel1D --embed_size=200 --notrainable --is_retrain --max_len=150
-python train.py --classifier=textcnn.TextCNNMultiKernel1D1 --embed_size=200 --notrainable --is_retrain --max_len=150
